f12969d6794a1e53a309b9b73bf3c9211d7832da
leannethng <leanne.thng@gmail.com>
2015-09-01 20:27:47 -0700

    clean up some of the spacing of html, css
    need to look into minifying my html, css, js to reduce file sizes

--- assets/javascript/fittext.js
+++ assets/javascript/fittext.js
@@ -1,44 +1,44 @@
 /*!
-* FitText.js 1.0 jQuery free version
-*
-* Copyright 2011, Dave Rupert http://daverupert.com
-* Released under the WTFPL license
-* http://sam.zoy.org/wtfpl/
-* Modified by Slawomir Kolodziej http://slawekk.info
-*
-* Date: Tue Aug 09 2011 10:45:54 GMT+0200 (CEST)
-*/
-(function(){
-  var css = function (el, prop) {
+ * FitText.js 1.0 jQuery free version
+ *
+ * Copyright 2011, Dave Rupert http://daverupert.com
+ * Released under the WTFPL license
+ * http://sam.zoy.org/wtfpl/
+ * Modified by Slawomir Kolodziej http://slawekk.info
+ *
+ * Date: Tue Aug 09 2011 10:45:54 GMT+0200 (CEST)
+ */
+(function() {
+  var css = function(el, prop) {
     return window.getComputedStyle ? getComputedStyle(el).getPropertyValue(prop) : el.currentStyle[prop];
   };
 
-  var addEvent = function (el, type, fn) {
+  var addEvent = function(el, type, fn) {
     if (el.addEventListener)
       el.addEventListener(type, fn, false);
-		else
-			el.attachEvent('on'+type, fn);
+    else
+      el.attachEvent('on' + type, fn);
   };
 
-  var extend = function(obj,ext){
-    for(var key in ext)
-      if(ext.hasOwnProperty(key))
+  var extend = function(obj, ext) {
+    for (var key in ext)
+      if (ext.hasOwnProperty(key))
         obj[key] = ext[key];
     return obj;
   };
 
-  window.fitText = function (el, kompressor, options) {
+  window.fitText = function(el, kompressor, options) {
 
     var settings = extend({
-      'minFontSize' : -1/0,
-      'maxFontSize' : 1/0
-    },options);
+      'minFontSize': -1 / 0,
+      'maxFontSize': 1 / 0
+    }, options);
 
-    var fit = function (el) {
+    var fit = function(el) {
       var compressor = kompressor || 1;
 
-      var resizer = function () {
-        el.style.fontSize = Math.max(Math.min(el.clientWidth / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)) + 'px';
+      var resizer = function() {
+        el.style.fontSize = Math.max(Math.min(el.clientWidth / (compressor * 10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)) + 'px';
       };
 
       // Call once to set.
@@ -51,7 +51,7 @@
     };
 
     if (el.length)
-      for(var i=0; i<el.length; i++)
+      for (var i = 0; i < el.length; i++)
         fit(el[i]);
     else
       fit(el);
--- assets/javascript/resize.js
+++ assets/javascript/resize.js
@@ -1,18 +1,17 @@
-
 //this triggers the resize when the window is pulled
-jQuery(window).resize(function(){
+jQuery(window).resize(function() {
   resize();
 });
 
 
 //the $ means on load
 $(function() {
-	resize();
+  resize();
 });
 
 
 //this is the funtion that is being used
 function resize() {
-	var _outerHeight = jQuery(window).height() - jQuery('#navbar').height();
-	jQuery('#top-section').height(_outerHeight + 40);
+  var _outerHeight = jQuery(window).height() - jQuery('#navbar').height();
+  jQuery('#top-section').height(_outerHeight + 40);
 }
